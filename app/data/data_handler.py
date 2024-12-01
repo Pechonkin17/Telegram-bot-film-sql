d5bcaeee1619958b79a2091a3def48b05348994b
Maksym <m.cipalici@gmail.com>
2024-12-01 16:11:21 +0200

    Update data_handler.py

--- app/data/data_handler.py
+++ app/data/data_handler.py
@@ -1,146 +1,102 @@
-from app.data.database import create_connection
+from .base import Session
+from .model import Film # , Genre
 
 
 
 def get_films():
-    connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM bot_data')
-    films = cursor.fetchall()
-    cursor.close()
-    connection.close()
-
-    film_dicts = []
-    for film in films:
-        film_dict = {
-            'id': film[0],
-            'title': film[1],
-            'fdescription': film[2],
-            'url': film[3],
-            'photo_url': film[4],
-            'rating': film[5]
-        }
-        film_dicts.append(film_dict)
-    return film_dicts
+    with Session() as session:
+        films = session.query(Film).all()
+        return [
+            {
+                'id': film.id,
+                'title': film.title,
+                'fdescription': film.fdescription,
+                'url': film.url,
+                'photo_url': film.photo_url,
+                'rating': film.rating
+            } for film in films
+        ]
 
 
 def get_film_by_title(title):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = "SELECT * FROM bot_data WHERE title = %s"
-    cursor.execute(query, (title,))
-    film = cursor.fetchone()
-    cursor.close()
-    connection.close()
-
-    if film is None:
-        return {}
-    else:
-        film_dict = {
-            'id': film[0],
-            'title': film[1],
-            'fdescription': film[2],
-            'url': film[3],
-            'photo_url': film[4],
-            'rating': film[5]
+    with Session() as session:
+        film = session.query(Film).filter(Film.title == title).first()
+        if not film:
+            return {}
+        return {
+            'id': film.id,
+            'title': film.title,
+            'fdescription': film.fdescription,
+            'url': film.url,
+            'photo_url': film.photo_url,
+            'rating': film.rating
         }
-        return film_dict
 
 
 def create_film(film: dict):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = """
-    INSERT INTO bot_data (title, fdescription, url, photo_url, rating)
-    VALUES (%s, %s, %s, %s, %s)
-    """
-    title = film.get('title')
-    fdescription = film.get('fdescription')
-    url = film.get('url')
-    photo_url = film.get('photo_url')
-    rating = film.get('rating')
-
-    cursor.execute(query, (title, fdescription, url, photo_url, rating))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with Session() as session:
+        new_film = Film(
+            title=film['title'],
+            fdescription=film['fdescription'],
+            url=film['url'],
+            photo_url=film['photo_url'],
+            rating=film['rating']
+        )
+        session.add(new_film)
+        session.commit()
 
 
 def delete_film(title):
-    if not find_film(title):
-        return False
-
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = "DELETE FROM bot_data WHERE title = %s"
-    cursor.execute(query,(title,))
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return True
+    with Session() as session:
+        film = session.query(Film).filter(Film.title == title).first()
+        if not film:
+            return False
+        session.delete(film)
+        session.commit()
+        return True
 
 
 def update_film(title, updated_film):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = """
-        UPDATE bot_data 
-        SET title = %s, fdescription = %s, url = %s, photo_url = %s, rating = %s 
-        WHERE title = %s
-        """
-    cursor.execute(query, (
-        updated_film.get('title'),
-        updated_film.get('fdescription'),
-        updated_film.get('url'),
-        updated_film.get('photo_url'),
-        updated_film.get('rating'),
-        title
-    ))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with Session() as session:
+        film = session.query(Film).filter(Film.title == title).first()
+        if not film:
+            return False
+
+        film.title = updated_film.get('title', film.title)
+        film.fdescription = updated_film.get('fdescription', film.fdescription)
+        film.url = updated_film.get('url', film.url)
+        film.photo_url = updated_film.get('photo_url', film.photo_url)
+        film.rating = updated_film.get('rating', film.rating)
+
+        session.commit()
+        return True
 
 
 def find_film(title):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = "SELECT * FROM bot_data WHERE title = %s"
-    cursor.execute(query, (title,))
-    film = cursor.fetchone()
-    cursor.close()
-    connection.close()
-
-    if film is None:
-        return None
-    else:
-        film_dict = {
-            'title': film[1],
-            'fdescription': film[2],
-            'url': film[3],
-            'photo_url': film[4],
-            'rating': film[5]
+    with Session() as session:
+        film = session.query(Film).filter(Film.title == title).first()
+        if not film:
+            return None
+        return {
+            'id': film.id,
+            'title': film.title,
+            'fdescription': film.fdescription,
+            'url': film.url,
+            'photo_url': film.photo_url,
+            'rating': film.rating
         }
-        return film_dict
 
 
-def find_films_by_partial_title(title):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = "SELECT * FROM bot_data WHERE title LIKE %s"
-    cursor.execute(query, ('%' + title + '%',))
-    films = cursor.fetchall()
-    cursor.close()
-    connection.close()
-
-    film_dicts = []
-    for film in films:
-        film_dict = {
-            'id': film[0],
-            'title': film[1],
-            'fdescription': film[2],
-            'url': film[3],
-            'photo_url': film[4],
-            'rating': film[5]
-        }
-        film_dicts.append(film_dict)
-    return film_dicts
+def find_films_by_partial_title(partial_title):
+    with Session() as session:
+        films = session.query(Film).filter(Film.title.like(f'%{partial_title}%')).all()
+        return [
+            {
+                'id': film.id,
+                'title': film.title,
+                'fdescription': film.fdescription,
+                'url': film.url,
+                'photo_url': film.photo_url,
+                'rating': film.rating
+            } for film in films
+        ]
