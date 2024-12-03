136f59fcb4ec06978f86a83b18f18df067a57fea
Maksym <m.cipalici@gmail.com>
2024-12-03 23:13:39 +0200

    fix

--- app/data/model.py
+++ app/data/model.py
@@ -6,15 +6,6 @@ from .base import Base
 
 
 
-# film_genre_table = Table(
-#     'film_genre',
-#     Base.metadata,
-#     Column('film_id', ForeignKey('films.id'), primary_key=True),
-#     Column('genre_id', ForeignKey('genres.id'), primary_key=True)
-# )
-
-
-
 class Film(Base):
     __tablename__ = 'films'
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -23,12 +14,3 @@ class Film(Base):
     url: Mapped[str] = mapped_column(String(300), nullable=False, unique=True)
     photo_url: Mapped[str] = mapped_column(String(1000), nullable=False, unique=True)
     rating: Mapped[int] = mapped_column(REAL, nullable=False, unique=False)
-    #genres: Mapped[List['Genre']] = relationship('Genre', secondary=film_genre_table, back_populates='films')
-
-
-
-# class Genre(Base):
-#     __tablename__ = 'films'
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-#     films: Mapped[List['Film']] = relationship('Film', back_populates='genre')
