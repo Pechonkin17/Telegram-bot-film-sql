16649628b7646747e5f359568cccbea8fd9b613d
Maksym <m.cipalici@gmail.com>
2024-12-03 22:59:40 +0200

    fix

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,2 +1,2 @@
-from .data_handler import get_films, get_film_by_title, create_film, delete_film, update_film, find_film, find_films_by_partial_title
+from .data_handler import get_films, get_film_by_id, create_film, delete_film, update_film, find_film, find_films_by_partial_title
 from .base import create_db, drop_db
